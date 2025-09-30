20b36574e54c4fe8ab1ab4095f599bf2174bc3ba
Michail Levykin <michailinlevyk@gmail.com>
2025-09-30 19:26:57 +0500

    added a timer for the duration of the number being entered

--- Home_work_1.py
+++ Home_work_1.py
@@ -1,6 +1,9 @@
 year = int(input("Введите год:"))
 
-if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
-    print ("Високосный год")
+if 1000 <= year <= 9999:
+    if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
+        print ("Високосный год")
+    else:
+        print ("Обычный год")
 else:
-    print ("Обычный год")
+    print("Ошибка! Введите четыёхзначное число")
