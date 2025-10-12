11a99ee8288d33462ca8f039252e964a7bcdb1ec
Michail Levykin <michailinlevyk@gmail.com>
2025-10-13 02:17:25 +0500

    Сделал запись имён в списки

---
+++ Home_work_4.py
@@ -0,0 +1,19 @@
+Boys = []
+Girls = []
+
+while True:
+    Boy_name = input("Введи мужское имя или End, если хочешь продолжить: ")
+    if Boy_name == "end":
+        break
+    Boys.append(Boy_name)
+    print("Мужские имена:" + str(Boys))
+
+while True:
+    Girl_name = input("Введи женское имя имя или End, если хочешь продолжить: ")
+    if Girl_name == "end":
+        break
+    Girls.append(Girl_name)
+    print("Женские имена:" + str(Girls))
+
+print("Мужские имена:" + str(Boys))
+print("Женские имена:" + str(Girls))
