21dd09e51bfa1ae1bd5dcbcaaf7cf32a68ff7d78
Michail Levykin <michailinlevyk@gmail.com>
2025-10-13 03:26:35 +0500

    Сделал полностью рабочую программу, которая определяет совместимость партнёров

--- Home_work_4.py
+++ Home_work_4.py
@@ -3,20 +3,27 @@ Girls = []
 
 while True:
     Boy_name = input("Введи мужское имя или End, если хочешь продолжить: ")
-    if Boy_name == "end":
+    if Boy_name.lower() == "end":
         break
     Boys.append(Boy_name)
     print("Мужские имена:" + str(Boys))
 
 while True:
     Girl_name = input("Введи женское имя имя или End, если хочешь продолжить: ")
-    if Girl_name == "end":
+    if Girl_name.lower() == "end":
         break
     Girls.append(Girl_name)
     print("Женские имена:" + str(Girls))
 
 Boys.sort()
 Girls.sort()
+count = len(Girls)
 
-print("Мужские имена:" + str(Boys))
-print("Женские имена:" + str(Girls))
+if len(Girls) == len(Boys):
+    print("\nРезультат")
+    print("Идеальные пары")
+    for i in range(len(Boys)):
+        print(Boys[i], "и", Girls[i])
+else:
+    print("\nРезультат")
+    print("Внимание, кто-то может остаться без пары.")
