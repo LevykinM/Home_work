1a25ab8327ea412b7a8a50d88f2cc54002dae6ec
Michail Levykin <michailinlevyk@gmail.com>
2025-11-03 18:05:44 +0500

    Добавил новую функцию, которая позволяет найти владельца документа по его номеру

--- Homework_6.py
+++ Homework_6.py
@@ -17,9 +17,9 @@ def get_shelf_by_number(number: str) -> str:
             return shelf
     return None
 
-def get_shelf_by_number(number: str) -> str:
-    """Возвращает номер полки, на которой лежит документ."""
-    for shelf, docs in directories.items():
-        if number in docs:
-            return shelf
+def get_owner_by_number(number: str) -> str:
+    """Возвращает имя владельца по номеру документа."""
+    for doc in documents:
+        if doc['number'] == number:
+            return doc['name']
     return None
